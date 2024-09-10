a82cd1262d1eba5f6170969078a74719106e8f08
psych0wh1z <ikeagudike@gmail.com>
2024-09-10 12:42:43 +0100

    Add test contract

---
+++ SMART-CONTRACTS/contracts/SmartBox.sol
@@ -0,0 +1,23 @@
+// SPSX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+contract SmartBox {
+
+    struct Lesson {
+
+    }
+
+    mapping(address => Lesson[]);
+
+    function addLesson()  returns () {
+        
+    }
+
+    function deleteLesson()  returns () {
+        
+    }
+
+    function updateLesson () returns() {
+        
+    }
+}

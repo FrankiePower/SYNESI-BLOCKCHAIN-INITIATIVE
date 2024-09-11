7ea6f7b891128dae0cb979e507540f48ee51b67d
DIKE <ikeagudike@gmail.com>
2024-09-11 03:14:10 +0100

    Update wagmi.ts

--- FRONT-END/src/wagmi.ts
+++ FRONT-END/src/wagmi.ts
@@ -1,9 +1,9 @@
 import { getDefaultConfig } from "@rainbow-me/rainbowkit";
-import { baseSepolia } from "wagmi/chains";
+import { baseSepolia, liskSepolia } from "wagmi/chains";
 
 export const config = getDefaultConfig({
   appName: "RainbowKit App",
   projectId: "c59e86f04bdb6a22343b9337956b2368",
-  chains: [baseSepolia],
+  chains: [baseSepolia, liskSepolia],
   ssr: true,
 });
